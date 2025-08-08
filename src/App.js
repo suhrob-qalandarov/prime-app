05637c78d522c7769822e7e08b6ed549d15bc88a
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-08 20:28:31 +0500

    Add routing with `react-router-dom` and create basic components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import {Route, Routes} from "react-router-dom";
+import {Main, Login} from "./components";
+
 const App = () => {
     return (
-        <>App</>
+        <div>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </div>
     )
 }
 
