d32aae55dc4d7b9603a9e1ebfbab220b0a92b70a
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-11 21:42:10 +0500

    Refactor `Navbar` to use MUI's `Stack` for improved structure; adjust styles in `navbar.css` and update padding for better alignment.

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -2,35 +2,38 @@ import {Link} from "react-router-dom";
 import NavbarIcons from "./navbar/navbar-icons";
 import NavbarLinks from "./navbar/navbar-links";
 import './navbar.css'
+import {Stack} from "@mui/material";
 
 const Navbar = () => {
     return (
-        <div class = 'container' className="main-header" id="main-header"
-             sx={{
-                 position: 'sticky',
-                 top: 0,
-                 zIndex: 999
-        }}>
-            <div className="container-custom">
-                <div className="row align-items-center">
-                    <Link to={'/'} className="col-lg-3">
-                        <h1
-                            id="main-logo"
-                            style={{
-                                fontWeight: 700,
-                                fontSize: "24px",
-                                margin: 0,
-                                color: "rgba(195,164,9,0.84)",
-                            }}
-                        >
-                            PRIME<span style={{ color: "#ff6b6b" }}>77</span>
-                        </h1>
-                    </Link>
-                    <NavbarLinks />
-                    <NavbarIcons />
+        <Stack className='container' style={{backgroundColor: "#f0f0f0"}}>
+            <div className="main-header" id="main-header"
+                 sx={{
+                     position: 'sticky',
+                     top: 0,
+                     zIndex: 999
+                 }}>
+                <div className="container-custom">
+                    <div className="row align-items-center">
+                        <Link to={'/'} className="col-lg-3" style={{textDecoration: "none"}}>
+                            <h1
+                                id="main-logo"
+                                style={{
+                                    fontWeight: 700,
+                                    fontSize: "24px",
+                                    margin: 0,
+                                    color: "rgba(195,164,9,0.84)"
+                                }}
+                            >
+                                PRIME<span style={{color: "#ff6b6b"}}>77</span>
+                            </h1>
+                        </Link>
+                        <NavbarLinks/>
+                        <NavbarIcons/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Stack>
     );
 };
 
