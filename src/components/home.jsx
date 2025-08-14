2cf23dcdb1b624d0ba260039cb5703da55c1e525
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-14 22:22:08 +0500

    Refactor `BrandShowcase` to a dedicated component; update `home.jsx` and `constants` for cleaner structure, adjust brand section styles in `home.css` and `index.css`.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,98 +1,15 @@
-import {categories} from '../constants'
+import {categories, brands} from '../constants'
 import './home.css';
 import {Box, Container, Stack} from "@mui/material";
 import {Link} from "react-router-dom";
+import BrandShowcase from "./home/brand-showcase";
 
 const Home = () => {
 
     return (
         <div>
             {/* 3D Hero Showcase Section */}
-            <section className="hero-section">
-                <div className="hero-3d-showcase">
-                    {/* Brand Showcase */}
-                    <h1 className="hero-title">BREND KOLLEKSIYASI</h1>
-                    <div className="brand-showcase desktop-only">
-                        <div className="brand-item loropiana-logo">
-                            <img
-                                src="https://tw.loropiana.com/on/demandware.static/Sites-loropiana-b2c-sa-Site/-/default/dw00aaaf6a/images/LoroPiana-logo.svg"
-                                alt="Loro Piana"
-                            />
-                        </div>
-                        <div className="brand-item polo-logo">
-                            <img src="/images/brands/ralphlauren.svg" alt="Polo" />
-                        </div>
-                        <div className="brand-item louboutin-logo">
-                            <img src="/images/brands/louboutin-logo.svg" alt="Louboutin" />
-                        </div>
-                        <div className="brand-item hermes-logo">
-                            <img src="/images/brands/hermes-orange.png" alt="Hermes" />
-                        </div>
-                        <div className="brand-item lacoste-logo">
-                            <img src="/images/brands/lacoste-logo.svg" alt="Hermes" />
-                        </div>
-                        <div className="brand-item versace-medusa-logo">
-                            <img src="/images/brands/versace-medusa-logo.png" alt="Versace" />
-                        </div>
-                        <div className="brand-item brunello-logo">
-                            <img src="/images/brands/brunello_cucinelli-logo.png" alt="Brunello Cucinelli" />
-                        </div>
-                        <div className="brand-item burberry-logo">
-                            <img src="/images/brands/burberry-logo.png" alt="Burberry" />
-                        </div>
-                        <div className="brand-item balenciaga-logo">
-                            <img src="/images/brands/balenciaga-logo.png" alt="Balenciaga" />
-                        </div>
-                        <div className="brand-item levis-logo">
-                            <img src="/images/brands/levis-logo.png" alt="Levi's" />
-                        </div>
-                        <div className="brand-item moncler-logo">
-                            <img src="/images/brands/moncler-logo.jpg" alt="Moncler" />
-                        </div>
-                        <div className="brand-item dolce-gabbana-logo">
-                            <img src="/images/brands/dolce-gabbana-logo.png" alt="Dolce & Gabbana" />
-                        </div>
-                        <div className="brand-item louis_vuitton-logo">
-                            <img src="/images/brands/louis_vuitton-logo.svg" alt="Louis Vuitton" />
-                        </div>
-                        <div className="brand-item zegna-logo">
-                            <img src="/images/brands/zegna-logo.svg" alt="Zegna" />
-                        </div>
-                    </div>
-
-                    {/* Mobile Brand Carousel */}
-                    {/*<div className="mobile-brand-carousel mobile-only">
-                        <div className="mobile-brand-track" id="mobileBrandTrack">
-                            {[
-                                "https://tw.loropiana.com/on/demandware.static/Sites-loropiana-b2c-sa-Site/-/default/dw00aaaf6a/images/LoroPiana-logo.svg",
-                                "/images/polo-ralph-lauren.png",
-                                "/images/louboutin-logo.svg",
-                                "/images/hermes-orange.png",
-                                "/images/versace-medusa-logo.png",
-                                "/images/brunello_cucinelli-logo.png",
-                                "/images/burberry-logo.png",
-                                "/images/balenciaga-logo.png",
-                                "/images/levis-logo.png",
-                                "/images/moncler-logo.jpg",
-                                "/images/dolce-gabbana-logo.png",
-                                "/images/louis_vuitton-logo.svg",
-                                "/images/zegna-logo.svg",
-                                // Duplicate items for loop
-                                "https://tw.loropiana.com/on/demandware.static/Sites-loropiana-b2c-sa-Site/-/default/dw00aaaf6a/images/LoroPiana-logo.svg",
-                                "/images/polo-ralph-lauren.png",
-                                "/images/louboutin-logo.svg",
-                                "/images/hermes-orange.png",
-                                "/images/versace-medusa-logo.png",
-                                "/images/brunello_cucinelli-logo.png",
-                            ].map((src, i) => (
-                                <div className="mobile-brand-item" key={i}>
-                                    <img src={src} alt="Brand" />
-                                </div>
-                            ))}
-                        </div>
-                    </div>*/}
-                </div>
-            </section>
+            {/*<BrandShowcase />*/}
 
             {/* Spotlight Categories Section */}
             <Stack className="category-section">
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,47 +12,20 @@ export const categories = [
 
 export const brands = [
     {
-        name : 'loropiana-logo',
-        image : 'https://tw.loropiana.com/on/demandware.static/Sites-loropiana-b2c-sa-Site/-/default/dw00aaaf6a/images/LoroPiana-logo.svg'
+        name: "Loro Piana",
+        src: "https://tw.loropiana.com/on/demandware.static/Sites-loropiana-b2c-sa-Site/-/default/dw00aaaf6a/images/LoroPiana-logo.svg",
     },
-
-    {
-        name : 'polo-logo',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-
-    {
-        name : '',
-        image : ''
-    },
-]
+    { name: "Polo", src: "/images/brands/ralphlauren.svg" },
+    { name: "Louboutin", src: "/images/brands/louboutin-logo.svg" },
+    { name: "Hermes", src: "/images/brands/hermes-orange.png" },
+    { name: "Lacoste", src: "/images/brands/lacoste-logo.svg" },
+    { name: "Versace", src: "/images/brands/versace-medusa-logo.png" },
+    { name: "Brunello Cucinelli", src: "/images/brands/brunello_cucinelli-logo.png" },
+    { name: "Burberry", src: "/images/brands/burberry-logo.png" },
+    { name: "Balenciaga", src: "/images/brands/balenciaga-logo.png" },
+    { name: "Levi's", src: "/images/brands/levis-logo.png" },
+    { name: "Moncler", src: "/images/brands/moncler-logo.jpg" },
+    { name: "Dolce & Gabbana", src: "/images/brands/dolce-gabbana-logo.png" },
+    { name: "Louis Vuitton", src: "/images/brands/louis_vuitton-logo.svg" },
+    { name: "Zegna", src: "/images/brands/zegna-logo.svg" },
+];
