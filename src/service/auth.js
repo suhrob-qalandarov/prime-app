f59b48dee6df1f7e5785bc47152af450c4f50a09
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-23 22:27:09 +0500

    Update auth endpoint path in `login` method for consistency and clarity.

--- src/service/auth.js
+++ src/service/auth.js
@@ -2,7 +2,7 @@ import axios from'./api'
 
 const AuthService = {
     async login (code) {
-        const response = await  axios.post(`/auth/${code}`)
+        const response = await  axios.post(`/auth/code/${code}`)
         return response.data
     }
 }
