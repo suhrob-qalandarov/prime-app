a0333aa906bc63b7539ed2d02c4c3ee7ad4af8a6
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-24 19:57:21 +0500

    Update `Profile` component: add hover and focus styles to improve button interactions.

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -125,6 +125,14 @@ const Profile = () => {
                                         marginTop: "-100px",
                                         left: "160px",
                                         color: "var(--burgundy-color)",
+                                        "&:hover": {
+                                            backgroundColor: "transparent",
+                                            outline: "none",
+                                            boxShadow: "none",
+                                        },
+                                        "&:focus": {
+                                            outline: "none",
+                                        },
                                     }}
                                 >
                                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 256 256"><path d="M124,216a12,12,0,0,1-12,12H48a12,12,0,0,1-12-12V40A12,12,0,0,1,48,28h64a12,12,0,0,1,0,24H60V204h52A12,12,0,0,1,124,216Zm108.49-96.49-40-40a12,12,0,0,0-17,17L195,116H112a12,12,0,0,0,0,24h83l-19.52,19.51a12,12,0,0,0,17,17l40-40A12,12,0,0,0,232.49,119.51Z"></path></svg>
