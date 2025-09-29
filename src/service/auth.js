cd151707dd6641aa1a61ed3c4a719fcd3ecd6f69
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-29 23:47:12 +0500

    Update `AuthService`: add `me` method for fetching user data and storing it in localStorage

--- src/service/auth.js
+++ src/service/auth.js
@@ -4,7 +4,12 @@ const AuthService = {
     async login (code) {
         const response = await  axios.post(`/auth/code/${code}`)
         return response.data
-    }
+    },
+
+    async me (userId) {
+        const response = await  axios.get(`/user/${userId}`);
+        localStorage.setItem("prime-user", JSON.stringify(response.data))
+    },
 }
 
 export default AuthService
