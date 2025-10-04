ac1f7a0b5913874f97fab918d40f7fb66263b4b9
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-04 22:24:12 +0500

    Remove unused `useState` import from `CategoriesList` component.

--- src/components/pages/catalog/categories-list.jsx
+++ src/components/pages/catalog/categories-list.jsx
@@ -1,8 +1,6 @@
 import { Box, Button } from "@mui/material"
-import {useState} from "react";
 
 const CategoriesList = ({ categories, onCategorySelect, selectedCategory }) => {
-
     return (
         <Box
             sx={{
