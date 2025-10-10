8f71eeb22e8f6eb815731b620fc0a9373820590f
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-10 23:30:02 +0500

    Add `urls.js` to centralize and manage application URLs

---
+++ src/constants/urls.js
@@ -0,0 +1,7 @@
+const urls = {
+    apiBaseUrl: 'https://api.howdy.uz/api/v1/', // Backend API base URL (update for production)
+    mainSiteUrl: 'https://prime.howdy.uz', // Main e-commerce site URL (update for production)
+    adminDashboardUrl: 'https://admin.howdy.uz', // Admin dashboard site URL (update for production)
+};
+
+export default urls;
