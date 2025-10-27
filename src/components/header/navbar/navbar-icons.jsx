be5ef57c3cd990f18466420719723f0af9130740
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-27 12:32:33 +0500

    Redesigned navbar icons using tailwind, removed css

--- src/components/header/navbar/navbar-icons.jsx
+++ src/components/header/navbar/navbar-icons.jsx
@@ -1,12 +1,8 @@
-"use client"
-
 import { Link } from "react-router-dom"
 import { useState } from "react"
 import SearchModal from "../../modals/search-modal"
 import CartModal from "../../modals/cart-modal"
-import { Box, Stack, Typography } from "@mui/material"
 import { SearchIcon, ProfileIcon, CartIcon } from "../../../icons"
-import "./navbar-icons.css"
 
 const NavbarIcons = () => {
     const [modal, setModal] = useState(null)
@@ -14,28 +10,40 @@ const NavbarIcons = () => {
 
     return (
         <>
-            <Stack className="col-lg-3">
-                <Box className="d-flex justify-content-end align-items-center">
-                    <Typography className="header-icons">
-                        <button className="header-icon" onClick={() => setModal("search")}>
+            <div className="hidden lg:flex col-span-3">
+                <div className="flex justify-end items-center w-full">
+                    <div className="flex items-center gap-4">
+                        <button
+                            className="p-2 text-[#8b1538]"
+                            onClick={() => setModal("search")}
+                        >
                             <SearchIcon />
                         </button>
 
-                        <span className="header-separator">|</span>
+                        <span className="text-[#8b1538] text-3xl font-light">|</span>
 
-                        <Link to={isLoggedIn ? "/profile" : "/login"} className="header-icon header-icon-profile">
+                        <Link
+                            to={isLoggedIn ? "/profile" : "/login"}
+                            className="p-2 text-[#8b1538] no-underline"
+                        >
                             <ProfileIcon />
                         </Link>
 
-                        <button className="header-icon" onClick={() => setModal("cart")}>
+                        <button
+                            className="p-2 text-[#8b1538] relative"
+                            onClick={() => setModal("cart")}
+                        >
                             <CartIcon />
-                            <span className="badge" id="cartBadge">
-                0
-              </span>
+                            <span
+                                className="absolute top-0 right-1 bg-[#a01b47] text-white text-xs rounded-full w-4 h-5 flex items-center justify-center font-semibold"
+                                id="cartBadge"
+                            >
+                                0
+                            </span>
                         </button>
-                    </Typography>
-                </Box>
-            </Stack>
+                    </div>
+                </div>
+            </div>
 
             <SearchModal isOpen={modal === "search"} onClose={() => setModal(null)} />
             <CartModal isOpen={modal === "cart"} onClose={() => setModal(null)} />
