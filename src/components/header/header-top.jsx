6de55f93d16592b52b50f2d3a0264bd335a850f7
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-28 11:08:45 +0500

    Redesigned header-top.jsx component

--- src/components/header/header-top.jsx
+++ src/components/header/header-top.jsx
@@ -2,13 +2,13 @@ import { Box, Stack } from "@mui/material"
 
 const HeaderTop = () => {
     return (
-        <Stack sx={{ position: "sticky", top: "0", zIndex: 1001, backgroundColor: "#f8f9fb" }}>
+        <div className="fixed z-[999] w-full bg-[#f8f9fb]">
             <Box className="w-full overflow-hidden">
                 <div className="whitespace-nowrap text-red-500 text-sm font-semibold animate-marquee">
                     {Array(20).fill("SAYT HOZIRDA TEST REJIMIDA ISHLAYAPTI!  •  XATOLIK HAQIDA SUPPORTGA YOZING!").join(" • ")}
                 </div>
             </Box>
-        </Stack>
+        </div>
     )
 }
 
